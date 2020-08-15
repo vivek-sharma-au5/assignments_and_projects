4e7e34ea6f5491bfda422ff294d58d9a0d02ee43
vivek-sharma-au5 <vivekxiix@gmail.com>
2020-08-15 21:29:27 +0530

    app push

--- assignment_onwerstown/index.js
+++ assignment_onwerstown/index.js
@@ -3,6 +3,8 @@ const app = express();
 const cors = require("cors");
 const mainRouter = require("./routes/programRoute");
 const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+dotenv.config();
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -10,4 +12,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/", mainRouter);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 module.exports = app;

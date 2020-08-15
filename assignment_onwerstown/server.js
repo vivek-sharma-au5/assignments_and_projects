8caefb1efe108a76baeb34e92f7243b8b476084a
vivek-sharma-au5 <vivekxiix@gmail.com>
2020-08-15 21:41:48 +0530

    final push

--- assignment_onwerstown/server.js
+++ assignment_onwerstown/server.js
@@ -1,7 +1,22 @@
-const http = require("http");
-const app = require("./index");
-let server = http.createServer(app);
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mainRouter = require("./routes/programRoute");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+dotenv.config();
 
-server.listen(6060, function () {
-  console.log("Server started at 6060");
-});
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/", mainRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+app.listen(process.env.PORT || 6060, console.log("Server started at 6060"));
